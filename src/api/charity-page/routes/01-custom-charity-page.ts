b187839b7b1f5223fffab81c2308c7d8c97365aa
Abdullah Fiaz <itsabdullahfiaz@gmail.com>
2026-04-13 16:55:27 +0500

    Add listPublished method and route for Charity Page API
    - Implemented a new method 'listPublished' in the Charity Page controller to retrieve published charity pages, including their titles and slugs.
    - Added a custom route '/charity-pages/navigation' to access the list of published charity pages, enhancing content retrieval capabilities.

--- src/api/charity-page/routes/01-custom-charity-page.ts
+++ src/api/charity-page/routes/01-custom-charity-page.ts
@@ -2,6 +2,16 @@ export default {
   routes: [
     {
       method: 'GET',
+      path: '/charity-pages/navigation',
+      handler: 'charity-page.listPublished',
+      config: {
+        auth: false,
+        policies: [],
+        middlewares: [],
+      },
+    },
+    {
+      method: 'GET',
       path: '/charity-pages/by-slug/:slug',
       handler: 'charity-page.findBySlug',
       config: {
