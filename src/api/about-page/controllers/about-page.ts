d0d4578807c334265cd1dd40e539bae8c2baedf4
Abdullah Fiaz <itsabdullahfiaz@gmail.com>
2026-04-20 21:50:26 +0500

    Add About Page API and Components
    - Created a new API for the About Page, including controller, service, and routes for managing content.
    - Added schema for the About Page with attributes for various sections including hero video, statement, differentiators, services, ecosystem, clients, and press.
    - Introduced multiple components for the About Page, such as Clients Section, Differentiators Section, Ecosystem Section, Press Section, Services Section, and Statement Section, each with defined attributes for enhanced content management.

---
+++ src/api/about-page/controllers/about-page.ts
@@ -0,0 +1,22 @@
+import { factories } from '@strapi/strapi';
+
+const UID = 'api::about-page.about-page' as any;
+
+export default factories.createCoreController(UID, () => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        heroVideo: true,
+        statementSection: true,
+        differentiatorsSection: { populate: ['items'] },
+        servicesSection: { populate: ['stories', 'video'] },
+        ecosystemSection: { populate: ['image'] },
+        clientsSection: { populate: ['logos'] },
+        pressSection: { populate: ['featureImage'] },
+      },
+    };
+
+    return await super.find(ctx);
+  },
+}));
---
+++ src/api/about-page/services/about-page.ts
@@ -0,0 +1,5 @@
+import { factories } from '@strapi/strapi';
+
+const UID = 'api::about-page.about-page' as any;
+
+export default factories.createCoreService(UID);
